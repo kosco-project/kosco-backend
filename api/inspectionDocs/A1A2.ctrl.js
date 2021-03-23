f38eeb0fb9d25be4c620efa5db6ecd6b12a51b69
Kim-Jaemin420 <jany240@naver.com>
2021-03-23 10:51:25 +0900

    feat: A1, A2 문서의 D1 임시저장 api 구현

--- api/inspectionDocs/A1A2.ctrl.js
+++ api/inspectionDocs/A1A2.ctrl.js
@@ -6,18 +6,22 @@ require('dotenv').config();
 require('date-utils');
 
 exports.save = async (req, res) => {
+  const token = req.headers.authorization.slice(7);
+  const ID = jwt.decode(token).userId;
   const { category } = req.params;
   const { H, D1 } = req.body;
-  const { CERTDT, VESSELNM, ID } = H;
   const date = new Date();
+  const { CERTDT, VESSELNM } = H;
 
   const pool = await sql.connect(config);
-  const { recordset } = await pool.request().query`
+  const { recordset: INITIAL_CERTNO } = await pool.request().query`
   SELECT dbo.GD_F_NO('CT','002001','${new Date().toFormat('YYYYMMDD')}','${ID}')
   `;
 
   if (category === 'A1' || category === 'A2') {
     try {
+      // A1_H
+
       // await sql.connect(config);
       // const isCertno = await sql.query`select CERTNO from GSVC_A1_H`;
 
@@ -29,26 +33,53 @@ exports.save = async (req, res) => {
     `);
 
       if (!CERTNO.length) {
-        const result1 = await pool
+        await pool
           .request()
-          .input('input_parameter', sql.NChar, category)
-          .input('CERTNO', sql.NChar, recordset[0][''])
+          .input('category', sql.NChar, category)
+          .input('CERTNO', sql.NChar, INITIAL_CERTNO[0][''])
           .input('CERTDT', sql.NChar, CERTDT)
           .input('VESSELNM', sql.NChar, VESSELNM)
           .input('date', sql.DateTime, date)
           .input('ID', sql.NChar, ID)
           .query(`insert GSVC_${category}_H(CERTNO, CERTDT, VESSELNM, IN_ID, UP_ID) values(@CERTNO, @CERTDT, @VESSELNM, @ID, @ID)`);
 
-        console.log(result1);
+        Object.values(D1).forEach(async (v, i) => {
+          await pool
+            .request()
+            .input('ID', sql.NChar, ID)
+            .input('category', sql.NChar, category)
+            .input('CERTNO', sql.NChar, INITIAL_CERTNO[0][''])
+            .input('value', sql.NChar, v)
+            .query(`insert GSVC_${category}_D1(CERTNO, CERTSEQ, Value, IN_ID, UP_ID) values(@CERTNO, ${i + 1}, @value, @ID, @ID)`);
+        });
 
         res.status(200).send();
       } else {
         await pool
           .request()
-          .input('input_parameter', sql.NChar, category)
+          .input('category', sql.NChar, category)
           .input('ID', sql.NChar, ID)
           .input('date', sql.DateTimeOffset, date)
           .query(`update GSVC_${category}_H set UP_ID = @ID, UP_DT = @date`);
+
+        const { recordset: D1DATA } = await pool.request().input('category', sql.NChar, category).query(`
+          SELECT Value from GSVC_${category}_D1
+        `);
+
+        Object.values(D1).forEach(async (v, i) => {
+          if (D1DATA[i].Value === v) {
+            await pool
+              .request()
+              .input('ID', sql.NChar, ID)
+              .input('date', sql.DateTimeOffset, date)
+              .input('certseq', sql.NChar, i + 1)
+              .input('category', sql.NChar, category)
+              .input('value', sql.NChar, v).query(`
+              select * from GSVC_${category}_D1 where CERTSEQ = @certseq
+              update GSVC_${category}_D1 set UP_ID = @ID, UP_DT = @date, Value = @value where CERTSEQ = @certseq
+              `);
+          }
+        });
 
         res.status(200).send();
       }

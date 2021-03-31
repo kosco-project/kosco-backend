86d05a312eb0d6b24d3b6cea19c9981613256fd5
dongchan-K <ehdcks1028@gmail.com>
2021-03-31 13:20:52 +0900

    feat: C 문서 임시저장 및 검사완료 api 구현

--- api/inspectionDocs/index.js
+++ api/inspectionDocs/index.js
@@ -5,11 +5,14 @@ const A3 = require('./A3.ctrl');
 const B1 = require('./B1.ctrl');
 const B3 = require('./B3.ctrl');
 
+const C = require('./C.ctrl');
+
 const router = express.Router();
 
 router.post('/A3/inspection/:type', A3.inspection);
 router.post('/B1/save', B1.save);
 router.post('/B3/inspection/:type', B3.inspection);
+router.post('/C/inspection/:type', C.inspection);
 
 router.post('/:category/save', A1A2.save);
 router.post('/:category/complete', A1A2.complete);

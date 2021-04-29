aaf6ccb9dbebdfef99199f67b1496a13fa3be86e
Kim-Jaemin420 <jany240@naver.com>
2021-04-29 13:17:55 +0900

    fix: A1, A2 카테고리 형식을 다른 문서와 동일하게 수정, cert-no 문제 수정

--- api/inspectionDocs/A1A2.ctrl.js
+++ api/inspectionDocs/A1A2.ctrl.js
@@ -5,19 +5,19 @@ const config = require('../../lib/configDB');
 require('dotenv').config();
 require('date-utils');
 
-const updateQuery = async (table, data, category, CERTNO, ID, VESSELNM, CERTDT) => {
+const updateQuery = async (table, data, path, CERTNO, ID, VESSELNM, CERTDT) => {
   const pool = await sql.connect(config);
 
   Object.values(data).forEach(async (v, i) => {
     await pool
       .request()
-      .input('category', sql.NChar, category)
+      .input('path', sql.NChar, path)
       .input('ID', sql.NChar, ID)
       .input('VESSELNM', sql.NChar, VESSELNM)
       .input('CERTSEQ', sql.NChar, i + 1)
-      .input('CERTNO', sql.NChar, CERTNO[0][''])
+      .input('CERTNO', sql.NChar, CERTNO)
       .input('value', sql.NChar, v).query(`
-      MERGE INTO GSVC_${category}_H
+      MERGE INTO GSVC_${path}_H
         USING (values (1)) AS Source (Number)
         ON (CERTNO IS NOT NULL)
         WHEN MATCHED THEN
@@ -25,7 +25,7 @@ const updateQuery = async (table, data, category, CERTNO, ID, VESSELNM, CERTDT) 
         WHEN NOT MATCHED THEN
           INSERT (CERTNO, CERTDT, VESSELNM, IN_ID, UP_ID) VALUES(@CERTNO, ${CERTDT}, @VESSELNM, ${ID}, ${ID});
 
-      MERGE INTO GSVC_${category}_${table}
+      MERGE INTO GSVC_${path}_${table}
         USING (values (1)) AS Source (Number)
         ON (CERTNO = @CERTNO AND CERTSEQ = @CERTSEQ)
         WHEN MATCHED AND (Value != @value) THEN
@@ -70,50 +70,59 @@ exports.details = async (req, res) => {
 };
 
 exports.inspection = async (req, res) => {
+  const path = req.path.split('/')[1];
   const token = req.headers.authorization.slice(7);
   const ID = jwt.decode(token).userId;
-  const { category, type } = req.params;
+  const { type } = req.params;
   const { H, D1, D2 } = req.body;
   const { VESSELNM, RCVNO } = H;
   const CERTDT = new Date().toFormat('YYYYMMDD');
 
   const pool = await sql.connect(config);
   const { recordset: CERTNO } = await pool.request().query`SELECT dbo.GD_F_NO('CT','002001',${CERTDT}, ${ID})`;
-  const { recordset: RcvNos } = await pool.request().query`SELECT RcvNo FROM GRCV_CT WHERE (RcvNo = ${RCVNO})`;
-  const RcvNo = RcvNos.map(({ RcvNo }) => RcvNo)[0];
+  // const { recordset: RcvNos } = await pool.request().query`SELECT RcvNo FROM GRCV_CT WHERE (RcvNo = ${RCVNO})`;
+  // const RcvNo = RcvNos.map(({ RcvNo }) => RcvNo)[0];
 
-  if (category === 'A1' || category === 'A2') {
-    try {
-      jwt.verify(token, process.env.JWT_SECRET);
-      if (type === 'save') {
-        // GRCV_CT 테이블에서 CERT_NO 삽입
-        await pool.request().query`
-          UPDATE GRCV_CT SET CERT_NO = ${CERTNO[0]['']}, UP_ID = ${ID}, UP_DT = getDate()
-          WHERE (RcvNo = ${RcvNo} AND Doc_No = ${category})
-        `;
-      } else {
-        // COMPLETE
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
 
-        // GRCV_CT 테이블에서 CERT_NO 삽입
-        await pool.request().query`
-          UPDATE GRCV_CT SET MagamYn = 1, MagamDt = ${CERTDT}, UP_ID = ${ID}, UP_DT = getDate()
-          WHERE (RcvNo = ${RcvNo} AND Doc_No = ${category})
-        `;
-      }
+    // H, D1, D2 데이터베이스 업데이트
+    await updateQuery('D1', D1, path, H.CERTNO || CERTNO[0][''], ID, VESSELNM, CERTDT);
+    await updateQuery('D2', D2, path, H.CERTNO || CERTNO[0][''], ID, VESSELNM, CERTDT);
 
-      await updateQuery('D1', D1, category, CERTNO, ID, VESSELNM, CERTDT);
-      await updateQuery('D2', D2, category, CERTNO, ID, VESSELNM, CERTDT);
+    if (type === 'save') {
+      // GRCV_CT 테이블에서 CERT_NO 삽입
+      await pool
+        .request()
+        .input('path', sql.NChar, path)
+        .input('CERTNO', sql.NChar, H.CERTNO || CERTNO[0][''])
+        .input('RCVNO', sql.NChar, RCVNO).query(`
+          UPDATE GRCV_CT SET CERT_NO = @CERTNO, UP_ID = ${ID}, UP_DT = getDate()
+          WHERE (RcvNo = @RCVNO AND Doc_No = @path)
+      `);
+    } else {
+      // COMPLETE
 
-      res.status(200).send();
-    } catch (e) {
-      console.error(e);
-      if (e.name === 'TokenExpiredError') {
-        return res.status(419).json({ code: 419, message: '토큰이 만료되었습니다.' });
-      }
-      if (e.name === 'JsonWebTokenError') {
-        return res.status(401).json({ code: 401, message: '유효하지 않은 토큰입니다.' });
-      }
-      res.status(500).send();
+      // GRCV_CT 테이블에서 CERT_NO 삽입
+      await pool
+        .request()
+        .input('CERTNO', sql.NChar, H.CERTNO || CERTNO[0][''])
+        .input('RCVNO', sql.NChar, RCVNO)
+        .input('path', sql.NChar, path).query(`
+          UPDATE GRCV_CT SET CERT_NO = @CERTNO, MagamYn = 1, MagamDt = ${CERTDT}, UP_ID = ${ID}, UP_DT = getDate()
+          WHERE (RcvNo = @RCVNO AND Doc_No = @path)
+      `);
     }
+
+    res.status(200).send();
+  } catch (e) {
+    console.error(e);
+    if (e.name === 'TokenExpiredError') {
+      return res.status(419).json({ code: 419, message: '토큰이 만료되었습니다.' });
+    }
+    if (e.name === 'JsonWebTokenError') {
+      return res.status(401).json({ code: 401, message: '유효하지 않은 토큰입니다.' });
+    }
+    res.status(500).send();
   }
 };
